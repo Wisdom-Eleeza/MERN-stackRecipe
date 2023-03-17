0121949b919119af18536937dfeca222ab5f5d3b
Wisdom-Eleeza <wisdom.eleeza@amalitech.com>
2023-03-17 13:35:28 +0000

    Working on food recipe to understand how the backend works

--- server/src/index.js
+++ server/src/index.js
@@ -3,11 +3,17 @@ import cors from "cors"; //cors allows the rules of communication between the fr
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 
+const Port = process.env.PORT || 3001
+
+dotenv.config()
+
 const app = express();
 
 app.use(express.json()); //this middleware is converting data send from the frontend to JSON format
 app.use(cors());
 
-mongoose.connect("DATABASE_CONNECT")
+mongoose.createConnection(process.env.DATABASE_CONNECT)
 
-app.listen(3001, () => console.log("SERVER STARTED"));
+app.listen(Port, () => {
+    console.log(`SERVER STARTED ON http://localhost: ${Port}`)
+  })
