5c18acffc2a407aef5cdfefe820a2ae67e4d672b
wisdom.eleeza@amalitech.com <wisdom.eleeza@amalitech.com>
2023-04-04 16:31:35 +0000

    Corrections done

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home";
 import { Auth } from "./pages/auth";
 import { CreateRecipe } from "./pages/create-recipe";
-import { SavedRecipe } from "./pages/saved-recipe";
+import { savedRecipe } from "./pages/saved-recipe.js";
 import { Navbar } from "./Components/navbar";
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/create-recipe" element={<CreateRecipe />} />
-          <Route path="/saved-recipe" element={<SavedRecipe />} />
+          <Route path="/saved-recipe" element={<savedRecipe />} />
         </Routes>
       </Router>
     </div>
